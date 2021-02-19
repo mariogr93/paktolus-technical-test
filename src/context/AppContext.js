f959aa424e113f5fa7182802f64fb00c81690f09
Mario Guadalupe <mguadalupe@farmatrix.com.do>
2021-02-19 17:00:14 -0400

    Created App Context

---
+++ src/context/AppContext.js
@@ -0,0 +1,52 @@
+import React, { useContext, createContext, useReducer } from "react";
+
+const AppContext = createContext();
+
+export function useAppContext() {
+  const context = useContext(AppContext);
+  if (!context) {
+    throw new Error("App Context must be used within AppProvider");
+  }
+  return context;
+}
+
+const initialState = {
+  user: {
+    id: "",
+    name: "",
+  },
+  money: 99.99,
+  gameHistory: [],
+  isAuthenticated: false,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "LOGIN":
+      return {
+        ...state,
+        user: action.payload.user,
+        isAuthenticated: action.payload.isAuthenticated,
+      };
+    case "LOGOUT":
+      return initialState;
+    case "INCREMENT_MONEY":
+      return { ...state, money: state.money + action.payload };
+    case "DECREMENT_MONEY":
+      return { ...state, money: state.money - 1 };
+    case "SET_GAME_HISTORY":
+      return { ...state, gameHistory: [...state.gameHistory, action.payload] };
+    default:
+      throw new Error(`Action type ${action.type} is not found`);
+  }
+}
+
+export function AppProvider(props) {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <AppContext.Provider value={{ state, dispatch }}>
+      {props.children}
+    </AppContext.Provider>
+  );
+}
